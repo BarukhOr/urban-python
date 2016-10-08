52ea6f0994ee81fa3fb375c44c12900b24b1398c
UtibeAbasi <1.BarukhOr@gmail.com>
2016-10-08 03:14:17 -0400

    Secret Message: Refactored Code
    + Code is a little more verbose, however simplified the method via which the files are renamed

--- Secret_Message/rename_files.py
+++ Secret_Message/rename_files.py
@@ -1,23 +1,21 @@
 import os
 
 def rename_files():
-    #(1) Get file names from a folder
-    file_list = os.listdir("/home/utibe/Desktop/Udacity/Secret_Message/prank")
+    #(1) Get file names from a folder and set current working directory
     directory = "/home/utibe/Desktop/Udacity/Secret_Message/prank"
+    file_list = os.listdir(directory)
+    print("Current working directory is "+os.getcwd())
+    print("Changing directory to the specified directory")
+    os.chdir(directory)
     
-    #print(file_list)
+    #(2) Get Number of files in that folder
     print("There are "+format(len(file_list))+" elements in the file list variable")
 
-    #(2) For each file, rename the filename
+    #(3) For each file, rename the filename
     digits = "01234567890"
-    #for file_name in file_list:
-    for file_name in os.listdir(directory):
+    for file_name in file_list:
         remove_digits = str.maketrans('','',digits)
         new_file_name = file_name.translate(remove_digits)
+        os.rename(file_name, new_file_name)
         
-        path = os.path.join(directory,file_name)
-        target = os.path.join(directory,new_file_name)
-
-        os.rename(path,target)
-
 rename_files()
