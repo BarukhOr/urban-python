ed84c6f5c669e7c27d4d2abb93cc897237e58416
UtibeAbasi <1.BarukhOr@gmail.com>
2016-10-08 03:19:41 -0400

    Secret Message: Refactored Code
    + Notify the user every time a file is renamed

--- Secret_Message/rename_files.py
+++ Secret_Message/rename_files.py
@@ -4,7 +4,9 @@ def rename_files():
     #(1) Get file names from a folder and set current working directory
     directory = "/home/utibe/Desktop/Udacity/Secret_Message/prank"
     file_list = os.listdir(directory)
-    print("Current working directory is "+os.getcwd())
+    saved_path=os.getcwd()
+    
+    print("Current working directory is "+saved_path)
     print("Changing directory to the specified directory")
     os.chdir(directory)
     
@@ -16,6 +18,10 @@ def rename_files():
     for file_name in file_list:
         remove_digits = str.maketrans('','',digits)
         new_file_name = file_name.translate(remove_digits)
+        print(file_name + " has been changed to " + new_file_name)
         os.rename(file_name, new_file_name)
-        
+
+    #(4) Revert changes to the current working directory just for 'safety' purposes
+    os.chdir(saved_path)
+    
 rename_files()
